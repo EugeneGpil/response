917dc2f141ee09ce26ed26dd1487e689bf7a09cf
EugeneGpil <gpilligrim@gmail.com>
2023-10-19 21:13:00 +0000

    1

--- app/modules/WriteValidationErrors/tests/should_write_errors_with_message_and_code/should_write_errors_with_message_and_code_test.go
+++ app/modules/WriteValidationErrors/tests/should_write_errors_with_message_and_code/should_write_errors_with_message_and_code_test.go
@@ -1,27 +1,29 @@
-package should_write_errors
+package should_write_errors_with_message_and_code
 
 import (
 	"fmt"
 	"net/http"
 	"testing"
 
-	"github.com/EugeneGpil/response/app/modules/WriteValidationErrors/vars"
 	"github.com/EugeneGpil/tester"
 
 	httpTester "github.com/EugeneGpil/httpTester"
 	responsePackage "github.com/EugeneGpil/response"
 )
 
+var mainErrorMessage = "Main error message"
 var property = "property"
 var responseError = "some error"
+var errorCode = 15
 var expectedMessage = fmt.Sprintf(
-	"{\"Message\":\"%v\",\"Errors\":{\"%v\":\"%v\"},\"Code\":0}",
-	vars.DefaultMessage,
+	"{\"Message\":\"%v\",\"Errors\":{\"%v\":\"%v\"},\"Code\":%v}",
+	mainErrorMessage,
 	property,
 	responseError,
+	errorCode,
 )
 
-func Test_should_write_errors(t *testing.T) {
+func Test_should_write_errors_with_message_and_code(t *testing.T) {
 	tester.SetTester(t)
 
 	errors := map[string]string{
@@ -33,7 +35,9 @@ func Test_should_write_errors(t *testing.T) {
 	response := responsePackage.New(writer)
 
 	err := response.WriteValidationErrors(responsePackage.ValidationErrorsDto{
-		Errors: errors,
+		Message: mainErrorMessage,
+		Errors:  errors,
+		Code:    errorCode,
 	})
 
 	tester.AssertNil(err)
